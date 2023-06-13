7580dc7437c68a88c49fc0d60d47fc553bbd2e78
AnuraagChetia <anuraagchetia77@gmail.com>
2023-06-13 21:36:25 +0530

    2nd commit

--- first-app.js
+++ first-app.js
@@ -1,1 +1,12 @@
-console.log("Hello world");
+const product = (i, j) => {
+  return i * j;
+};
+console.log(product(2, 3));
+const student = {
+  name: "Anuraag",
+  age: "21",
+  greet() {
+    return "Hello" + this.name;
+  },
+};
+console.log(student.greet());
