39f11ce11b90007c1b9a58093a67693fdbc93eeb
AnuraagChetia <anuraagchetia77@gmail.com>
2023-06-13 23:07:07 +0530

    Understanding the basics

--- first-app.js
+++
@@ -1,8 +0,0 @@
-const fruits = ["apple", "oranges", " ", "mango", " ", "lemon"];
-const newFruits = fruits.map((fruit) => {
-  if (fruit === " ") {
-    fruit = "empty string";
-  }
-  return fruit;
-});
-console.log(newFruits);
