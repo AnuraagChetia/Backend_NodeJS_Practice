85970f9fd1af979f4eacd3a66acd7c4f7cec09cf
AnuraagChetia <anuraagchetia77@gmail.com>
2023-06-14 21:22:15 +0530

    Intro to Middlewares

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require("express");
 
+const bodyParser = require("body-parser");
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("Inside middleware");
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    `<form action="/product" method="POST"><input type='text' name='title'/><input type='text' name='size'/><button type='submit'>Add Product</button></form>`
+  );
 });
 
-app.use((req, res, next) => {
-  console.log("Inside another middleware");
-  res.send("<h1>Welcome to Express!</h1>");
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Homw Page</h>");
 });
 
 app.listen(3000);
